5dbea2052a0fe7b20345c87ea8d10e4a98ff1abf
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-16 00:35:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,8 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet(planeteer_calls)# code an argument here
-  # Your code here
-  planeteer_calls.map! {|name| name.capitalize + "!" }
-end
+
+
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
